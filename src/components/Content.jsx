ff4bf835c38de5020049384896e9cd1a6755381c
Volodymyr Bubenko <>
2020-04-30 10:33:45 -0500

    Start Social Media

---
+++ src/components/Content.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div className="headerImage">
+        <img
+          className="mainImage"
+          src="https://thumbs.dreamstime.com/b/logo-wall-modern-office-editorial-conceptual-d-logo-facebook-wall-modern-office-editorial-conceptual-d-163585829.jpg"
+        />
+      </div>
+      <div>
+        My posts
+        <div>New Post</div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://somnomed.com/wp-content/uploads/2017/09/SM-SOCIAL-MEDIA-ICON-FACEBOOK-5.png"
+      />
+    </header>
+  );
+};
+
+export default Header;
