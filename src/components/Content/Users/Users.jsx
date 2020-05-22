fe99e972e710281a3013acb480c793422e996a22
Volodymyr Bubenko <>
2020-05-21 21:31:00 -0500

    User Page - Preloader

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -1,78 +1,81 @@
 import React from "react";
 import * as axios from "axios";
+import styles from "./users.module.css";
 
-class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((responce) => {
-        this.props.setUsers(responce.data.items);
-      });
+let Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  let pages = [];
+
+  for (let i = 1; i < pagesCount; i++) {
+    pages.push(i);
   }
 
-  // getUsers = () => {
-  //   if (this.props.users.length === 0) {
-  //     axios
-  //       .get("https://social-network.samuraijs.com/api/1.0/users")
-  //       .then((responce) => {
-  //         this.props.setUsers(responce.data.items);
-  //       });
-  //   }
-  // };
-
-  render() {
-    return (
-      <div>
-        {this.props.users.map((u) => (
-          <div key={u.id}>
-            <span>
-              <div>
-                <img
-                  src={
-                    u.photos.small != null
-                      ? u.photos.small
-                      : "https://picsum.photos/200/300"
-                  }
-                  alt=""
-                />
-              </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.pagination}>
+        {pages.map((p) => {
+          return (
+            <span
+              className={props.currentPage === p && styles.selectedPage}
+              onClick={(e) => {
+                props.onPageChanged(p);
+              }}
+            >
+              {p}
             </span>
-            <span>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    this.props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    this.props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </span>
-            <span>
-              <span>
-                <div>{u.name}</div>
-                <div>{u.status}</div>
-              </span>
-              <span>
-                {/* <div>{u.location.country}</div> */}
-                {/* <div>{u.location.city}</div> */}
-                <div>World</div>
-              </span>
-            </span>
-          </div>
-        ))}
+          );
+        })}
       </div>
-    );
-  }
-}
+
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={
+                  u.photos.small != null
+                    ? u.photos.small
+                    : "https://picsum.photos/200/300"
+                }
+                alt=""
+              />
+            </div>
+          </span>
+          <span>
+            {u.followed ? (
+              <button
+                onClick={() => {
+                  props.unfollow(u.id);
+                }}
+              >
+                Unfollow
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  props.follow(u.id);
+                }}
+              >
+                Follow
+              </button>
+            )}
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              {/* <div>{u.location.country}</div> */}
+              {/* <div>{u.location.city}</div> */}
+              <div>World</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Users;
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let Preloader = (props) => {
+  return (
+    <div>
+      <img src="https://flevix.com/wp-content/uploads/2020/01/Bounce-Bar-Preloader-1.gif" />
+    </div>
+  );
+};
+
+export default Preloader;
