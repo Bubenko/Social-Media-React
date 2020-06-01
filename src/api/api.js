e2920a2c77112c06087e694edc0b39b1a91de657
Volodymyr Bubenko <>
2020-06-01 17:22:16 -0500

    Add Data Acess Layer (DAL), axios.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "1b8b137b-2d80-49b2-99df-d9c42f42aa59",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
